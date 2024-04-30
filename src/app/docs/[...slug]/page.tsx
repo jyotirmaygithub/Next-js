93031507c11b44e481751df9b7083c99fbe80130
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-04-30 16:25:41 +0530

    not found

--- src/app/docs/[...slug]/page.tsx
+++ src/app/docs/[...slug]/page.tsx
@@ -1,17 +1,22 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
-export default function page({
+export default function Page({
   params,
 }: {
-  params: {
-    slug: string[];
+  params?: {
+    slug?: string[];
   };
 }) {
-  console.log("pararmas = ", params.slug);
-  if (params.slug?.length === 1) {
+  console.log("params =", params?.slug);
+  if (params?.slug?.length === 1) {
     return <h1>heading element is displaying when slug length is one</h1>;
-  } else if (params.slug?.length === 2) {
+  } else if (params?.slug?.length === 2) {
     return <p>paragraph element is displaying when slug length is 2</p>;
+  }
+  else if(params?.slug?.length == 3){
+    notFound()
+    // so this is the notfound method
   }
   return <h5>heading 5 element now</h5>;
 }
---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    //  so this is the way to create a not found page in next.js
+    // other than this we have a not found function  for the side of the nextjs
+    <div>
+      <h2>page not found</h2>
+      <p>404 error</p>
+    </div>
+  )
+}
+
